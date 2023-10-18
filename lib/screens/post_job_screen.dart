11c89e20a184f40f320451341c9b6536f2f60dd1
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2023-10-18 19:33:04 +0530

    Add Features

--- lib/screens/post_job_screen.dart
+++ lib/screens/post_job_screen.dart
@@ -3,10 +3,9 @@ import 'package:parttimenow_flutter/resources/auth_method.dart';
 import 'package:parttimenow_flutter/utils/colors.dart';
 import 'package:parttimenow_flutter/utils/utills.dart';
 import 'package:intl/intl.dart';
-// Import your AuthMethod class
 
 class PostJobScreen extends StatefulWidget {
-  const PostJobScreen({super.key});
+  const PostJobScreen({Key? key}) : super(key: key);
 
   @override
   State<PostJobScreen> createState() => _PostJobScreenState();
@@ -20,6 +19,25 @@ class _PostJobScreenState extends State<PostJobScreen> {
   final TextEditingController salaryController = TextEditingController();
   final TextEditingController locationController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
+
+  int descriptionLength = 0;
+
+  String? validateRequiredField(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'This field is required';
+    }
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    descriptionController.addListener(() {
+      setState(() {
+        descriptionLength = descriptionController.text.length;
+      });
+    });
+  }
 
   @override
   void dispose() {
@@ -41,7 +59,7 @@ class _PostJobScreenState extends State<PostJobScreen> {
       firstDate: DateTime(2000),
       lastDate: DateTime(2101),
     );
-    if (picked != null && picked != DateTime.now()) {
+    if (picked != null) {
       setState(() {
         controller.text = formatDate(picked);
       });
@@ -78,167 +96,277 @@ class _PostJobScreenState extends State<PostJobScreen> {
         title: const Text('Post a Job'),
         backgroundColor: mobileBackgroundColor,
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView(
-          children: [
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(
-                            right:
-                                8), // Add gap between Start Date and Start Time
-                        child:
-                            buildTextField('Start Date', startDateController),
+      body: SingleChildScrollView(
+        // Wrap the Scaffold in a SingleChildScrollView
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 8, top: 40),
+                          child: buildTextField(
+                              'Start Date', startDateController, _selectDate),
+                        ),
                       ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(
-                            left:
-                                8), // Add gap between Start Date and Start Time
-                        child:
-                            buildTextField('Start Time', startTimeController),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 8, top: 40),
+                          child: buildTextField(
+                              'Start Time', startTimeController, _selectTime),
+                        ),
                       ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            Column(
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(
-                            right: 8), // Add gap between End Date and End Time
-                        child: buildTextField('End Date', endDateController),
-                      ),
-                    ),
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(
-                            left: 8), // Add gap between End Date and End Time
-                        child: buildTextField('End Time', endTimeController),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
-            ),
-            const SizedBox(height: 10),
-            buildTextField('Salary', salaryController),
-            const SizedBox(height: 10),
-            buildTextField('Location', locationController),
-            const SizedBox(height: 10),
-            buildDescriptionField(),
-            const SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () async {
-                // Add a print statement to check if this code block is executed
-                logger.d("Post Job button pressed");
-
-                final startDate = DateTime.parse(startDateController.text);
-                final endDate = DateTime.parse(endDateController.text);
-                final salary = double.parse(salaryController.text);
-                final location = locationController.text;
-                final description = descriptionController.text;
-
-                await AuthMethod().postJob(
-                  startDate: startDate,
-                  endDate: endDate,
-                  salary: salary,
-                  location: location,
-                  description: description,
-                  startTime: startTimeController.text,
-                  endTime: endTimeController.text,
-                );
-
-                // Handle successful job posting
-                // Clear text controllers or navigate to another screen as needed
-                startDateController.clear();
-                startTimeController.clear();
-                endDateController.clear();
-                endTimeController.clear();
-                salaryController.clear();
-                locationController.clear();
-                descriptionController.clear();
-              },
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white, // Button background color
-                backgroundColor: mobileBackgroundColor, // Button text color
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10), // Rounded corners
-                ),
-                minimumSize: const Size(150, 40), // Set the button size
+                    ],
+                  ),
+                ],
               ),
-              child: const Text('Post Job', style: TextStyle(fontSize: 16)),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(right: 8),
+                          child: buildTextField(
+                              'End Date', endDateController, _selectDate),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          margin: const EdgeInsets.only(left: 8),
+                          child: buildTextField(
+                              'End Time', endTimeController, _selectTime),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 5),
+                ],
+              ),
+              const SizedBox(height: 20),
+              buildSalaryField(),
+              const SizedBox(height: 20),
+              buildLocationField(),
+              const SizedBox(height: 20),
+              buildDescriptionField(),
+              const SizedBox(height: 20),
+              ElevatedButton(
+                onPressed: () async {
+                  if (descriptionLength > 200) {
+                    showValidationError("Character limit exceeded!");
+                  } else {
+                    final validationError = getRequiredFieldsValidation();
+                    if (validationError != null) {
+                      showValidationError(validationError);
+                    } else {
+                      logger.d("Post Job button pressed");
+
+                      final startDate =
+                          DateTime.parse(startDateController.text);
+                      final endDate = DateTime.parse(endDateController.text);
+
+                      if (endDate.isBefore(startDate)) {
+                        showValidationError(
+                            "End date must be after or equal to start date");
+                      } else {
+                        final salary = double.parse(salaryController.text);
+                        final location = locationController.text;
+                        final description = descriptionController.text;
+
+                        await AuthMethod().postJob(
+                          startDate: startDate,
+                          endDate: endDate,
+                          salary: salary,
+                          location: location,
+                          description: description,
+                          startTime: startTimeController.text,
+                          endTime: endTimeController.text,
+                        );
+
+                        startDateController.clear();
+                        startTimeController.clear();
+                        endDateController.clear();
+                        endTimeController.clear();
+                        salaryController.clear();
+                        locationController.clear();
+                        descriptionController.clear();
+                      }
+                    }
+                  }
+                },
+                style: ElevatedButton.styleFrom(
+                  foregroundColor: Colors.white,
+                  backgroundColor: mobileBackgroundColor,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  minimumSize: const Size(150, 40),
+                ),
+                child: const Text('Post Job', style: TextStyle(fontSize: 16)),
+              ),
+            ],
+          ),
         ),
       ),
     );
   }
 
-  Widget buildTextField(String labelText, TextEditingController controller) {
+  void showValidationError(String error) {
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(error),
+        backgroundColor: Colors.red,
+      ),
+    );
+  }
+
+  String? getRequiredFieldsValidation() {
+    if (startDateController.text.isEmpty ||
+        startTimeController.text.isEmpty ||
+        endDateController.text.isEmpty ||
+        endTimeController.text.isEmpty ||
+        salaryController.text.isEmpty ||
+        locationController.text.isEmpty ||
+        descriptionController.text.isEmpty) {
+      final requiredFields = [
+        if (startDateController.text.isEmpty) "Start Date",
+        if (startTimeController.text.isEmpty) "Start Time",
+        if (endDateController.text.isEmpty) "End Date",
+        if (endTimeController.text.isEmpty) "End Time",
+        if (salaryController.text.isEmpty) "Salary",
+        if (locationController.text.isEmpty) "Location",
+        if (descriptionController.text.isEmpty) "Description",
+      ];
+      return "All the fields are required";
+    }
+    return null;
+  }
+
+  Widget buildTextField(String labelText, TextEditingController controller,
+      Function(BuildContext, TextEditingController) onTapFunction) {
     return TextField(
       controller: controller,
-      style: const TextStyle(
-          color: Colors.black, fontSize: 14), // Adjust font size
+      style: const TextStyle(color: Colors.black, fontSize: 14),
       decoration: InputDecoration(
         labelText: labelText,
         labelStyle: const TextStyle(
-            color: Colors.black, fontSize: 14), // Adjust font size
+          color: Colors.black,
+          fontSize: 14,
+        ),
         hintText: 'Enter a value',
         hintStyle: const TextStyle(color: Colors.grey),
         enabledBorder: OutlineInputBorder(
           borderSide: const BorderSide(color: Colors.black),
-          borderRadius: BorderRadius.circular(10), // Adjust field size
+          borderRadius: BorderRadius.circular(10),
         ),
         focusedBorder: OutlineInputBorder(
           borderSide: const BorderSide(color: Color.fromARGB(255, 206, 124, 0)),
-          borderRadius: BorderRadius.circular(10), // Adjust field size
+          borderRadius: BorderRadius.circular(10),
         ),
       ),
       onTap: () {
-        if (labelText.contains('Date')) {
-          _selectDate(context, controller);
-        } else if (labelText.contains('Time')) {
-          _selectTime(context, controller);
-        }
+        onTapFunction(context, controller);
       },
     );
   }
 
-  Widget buildDescriptionField() {
+  Widget buildSalaryField() {
     return TextField(
-      controller: descriptionController,
-      style: const TextStyle(
-          color: Colors.black, fontSize: 14), // Adjust font size
-      maxLines: 5,
+      controller: salaryController,
+      style: const TextStyle(color: Colors.black, fontSize: 14),
+      keyboardType: TextInputType.number,
       decoration: InputDecoration(
-        labelText: 'Description',
+        labelText: 'Salary',
         labelStyle: const TextStyle(
-            color: Colors.black, fontSize: 14), // Adjust font size
-        hintText: 'Enter a description',
+          color: Colors.black,
+          fontSize: 14,
+        ),
+        hintText: '30000',
         hintStyle: const TextStyle(color: Colors.grey),
         enabledBorder: OutlineInputBorder(
           borderSide: const BorderSide(color: Colors.black),
-          borderRadius: BorderRadius.circular(10), // Adjust field size
+          borderRadius: BorderRadius.circular(10),
         ),
         focusedBorder: OutlineInputBorder(
-          borderSide:
-              const BorderSide(color: Color.fromARGB(255, 255, 162, 22)),
-          borderRadius: BorderRadius.circular(10), // Adjust field size
+          borderSide: const BorderSide(color: Color.fromARGB(255, 206, 124, 0)),
+          borderRadius: BorderRadius.circular(10),
         ),
       ),
+    );
+  }
+
+  Widget buildLocationField() {
+    return TextField(
+      controller: locationController,
+      style: const TextStyle(color: Colors.black, fontSize: 14),
+      keyboardType: TextInputType.text,
+      decoration: InputDecoration(
+        labelText: 'Location',
+        labelStyle: const TextStyle(
+          color: Colors.black,
+          fontSize: 14,
+        ),
+        hintText: 'Enter a location',
+        hintStyle: const TextStyle(color: Colors.grey),
+        enabledBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Colors.black),
+          borderRadius: BorderRadius.circular(10),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: const BorderSide(color: Color.fromARGB(255, 206, 124, 0)),
+          borderRadius: BorderRadius.circular(10),
+        ),
+      ),
+    );
+  }
+
+  Widget buildDescriptionField() {
+    return Stack(
+      children: [
+        TextField(
+          controller: descriptionController,
+          style: const TextStyle(color: Colors.black, fontSize: 14),
+          maxLines: 6,
+          decoration: InputDecoration(
+            labelText: 'Description',
+            labelStyle: const TextStyle(
+              color: Colors.black,
+              fontSize: 14,
+            ),
+            hintText: 'Enter a description',
+            hintStyle: const TextStyle(color: Colors.grey),
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: Colors.black),
+              borderRadius: BorderRadius.circular(10),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+                color: descriptionLength <= 200
+                    ? Color.fromARGB(255, 255, 162, 22)
+                    : Colors.red,
+              ),
+              borderRadius: BorderRadius.circular(10),
+            ),
+          ),
+        ),
+        Positioned(
+          bottom: 8,
+          right: 8,
+          child: Text(
+            '${descriptionLength.toString()} / 200',
+            style: TextStyle(
+              color: descriptionLength <= 200 ? Colors.grey : Colors.red,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
